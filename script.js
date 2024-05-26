f148d3d8d3c2038be130082f5bd5332007a5b955
Shivan Kathir <shivan.kathir@gmail.com>
2024-05-26 17:09:26 -0400

    created populate display function which populates display screen of calculator based on user input

--- script.js
+++ script.js
@@ -13,7 +13,66 @@ function divide(a,b){
     return a/b;
 }
 
-let firstNum;
-let secondNum;
-let operator;
+let firstNum=null;
+let secondNum=null;
+let operator=null;
 
+
+
+function operate(a,b,operator){
+    if(operator==="+"){
+        display.textContent=add(a,b);
+    }
+    else if (operator==="-"){
+        display.textContent=subtract(a,b);
+    }
+    else if(operator==="*"){
+        display.textContent=multiply(a,b);
+    }
+    else if (operator==="/"){
+        display.textContent=divide(a,b);
+    }
+}
+
+
+
+
+const arrayOfOperators = ["+","-","*","/"];
+const display = document.querySelector(".display");
+const numbers = document.querySelectorAll(".number").forEach((index)=>{
+    index.addEventListener("click",()=>{
+        populateDisplay(index.textContent);
+    })
+});
+const operators = document.querySelectorAll(".operators").forEach((index)=>{
+    index.addEventListener("click",populateDisplay(index.textContent));
+})
+
+
+
+
+function populateDisplay(a){
+    if(a in arrayOfOperators&&firstNum!==null){
+        operator=a;
+        
+    }
+    if (firstNum===null&&operator===null){
+        firstNum=a;
+        display.textContent=firstNum;
+    }
+    else if(operator===null&&firstNum!==null){
+        firstNum=firstNum+a;
+        display.textContent=firstNum;
+    }
+    else if(operator!==null&&firstNum!==null&&secondNum===null){
+        secondNum=a;
+        display.textContent=secondNum;
+    }
+    else if(operator!==null&&firstNum!==null&&secondNum!==null){
+        secondNum=secondNum+a;
+        display.textContent=secondNum;
+    }
+    else if (a==="="&&operator!==null&&firstNum!==null&&secondNum!=null){
+        operate(firstNum,secondNum,operator);
+    }
+}
